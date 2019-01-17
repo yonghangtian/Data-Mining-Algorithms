80f82c8789bd7b5c084db94ff82553f37a98d4ee
yonghangtian <yonghangtian@gmail.com>
2019-01-17 14:52:07 +0800

    improve apriori.py

--- src/APriori/apriori.py
+++ src/APriori/apriori.py
@@ -33,41 +33,48 @@ def createC1(dataSet):
 
 
 def scanD(D,Ck,minSupport):
-    #参数：数据集、候选项集列表 Ck以及感兴趣项集的最小支持度 minSupport
+    # parameters: dataset, a list of candidate set: Ck, and the minmun support(minsupport).
     ssCnt={}
-    for tid in D:#遍历数据集
-        for can in Ck:#遍历候选项
-            if can.issubset(tid):#判断候选项中是否含数据集的各项
+    # traversing all tid(item lists) in dataset.
+    for tid in D:
+        # traversing all sets in candidate sets.
+        for can in Ck:
+            # check whether (set in candidate set) are subset of (item list in dataset).
+            if can.issubset(tid):
                 # python3 can not support
                 # if not ssCnt.has_key(can):
                 if not can in ssCnt:
-                    #不含设为1
+                    # initial with 1
                     ssCnt[can] = 1
-                #有则计数加1
+                # update the frequency of can
                 else: ssCnt[can] += 1
-    #数据集大小
+    # the size of dataset
     numItems = float(len(D))
-    #L1初始化
+    # initial Lk, which is a list of frequent item-set.
     retList = []
-    #记录候选项中各个数据的支持度
+    # store the support of each item-set
     supportData = {}
     for key in ssCnt:
-        # 计算支持度
+        # calculate the support, where support is between (0,1).
         support = ssCnt[key]/numItems
         if support >= minSupport:
-            # 满足条件加入L1中
+            # add frequent itemset to retList.
             retList.insert(0, key)
         supportData[key] = support
     return retList, supportData
 
 
-def aprioriGen(Lm, k): #组合，向上合并
-    # 参数：频繁项集列表 Lm (m = k-1) 与目标候选项集元素个数 k
-    #creates Ck
+
+def aprioriGen(Lm, k):
+    """
+    :param Lm: a list of frequent itemsets:Lm (m = k-1)
+    :param k:  k is the item size of Ck
+    :return: Ck, a list of candidate itemset of size k
+    """
+    # creates Ck, this retList is different from retList in func: scanD.
     retList = []
     lenLm = len(Lm)
     for i in range(lenLm):
-        # 两两组合遍历
         for j in range(i+1, lenLm):
             L1 = list(Lm[i])[:k-2]
             L2 = list(Lm[j])[:k-2]
@@ -89,7 +96,9 @@ def apriori(dataSet, minSupport = 0.5):
     L1, supportData = scanD(D, C1, minSupport)#单项最小支持度判断 0.5，生成L1
     L = [L1]
     k = 2
-    while (len(L[k-2]) > 0):#创建包含更大项集的更大列表,直到下一个大的项集为空
+    # 创建包含更大项集的更大列表,直到下一个大的项集为空
+    # continue to generate L(k+1) until last Lk is empty.
+    while (len(L[k-2]) > 0):
         #Ck
         Ck = aprioriGen(L[k-2], k)
         #get Lk
@@ -104,31 +113,33 @@ def generateRules(L, supportData, minConf=0.7):
     #频繁项集列表、包含那些频繁项集支持数据的字典、最小可信度阈值
     #存储所有的关联规则
     bigRuleList = []
-    #只获取有两个或者更多集合的项目，从1,即第二个元素开始，L[0]是单个元素的
+    #只获取有两个或者更多集合的项目，从1, 即第二个元素开始，L[0]是单个元素的
     for i in range(1, len(L)):
         # 两个及以上的才可能有关联一说，单个元素的项集不存在关联问题
         for freqSet in L[i]:
-            H1 = [frozenset([item]) for item in freqSet]
             #该函数遍历L中的每一个频繁项集并对每个频繁项集创建只包含单个元素集合的列表H1
+            H1 = [frozenset([item]) for item in freqSet]
             if (i > 1):
                 #如果频繁项集元素数目超过2,那么会考虑对它做进一步的合并
                 rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
             else:
-                #第一层时，后件数为1
-                # 调用函数2
+                # i = 1, which means that the size of frequent itemset is 2
+                # calcConf
                 calcConf(freqSet, H1, supportData, bigRuleList, minConf)
     return bigRuleList
 
 
 def calcConf(freqSet, H, supportData, brl, minConf=0.7):
     #针对项集中只有两个元素时，计算可信度
-    prunedH = []#返回一个满足最小可信度要求的规则列表
-    for conseq in H:#后件，遍历 H中的所有项集并计算它们的可信度值
+    #返回一个满足最小可信度要求的规则列表
+    prunedH = []
+    #后件，遍历 H中的所有项集并计算它们的可信度值
+    for conseq in H:
         #可信度计算，结合支持度数据
         conf = supportData[freqSet]/supportData[freqSet-conseq]
         if conf >= minConf:
             #如果某条规则满足最小可信度值,那么将这些规则输出到屏幕显示
-            print (freqSet-conseq,'-->',conseq,'conf:',conf)
+            print (freqSet-conseq,'-->', conseq, 'conf:', conf)
             #添加到规则里，brl 是前面通过检查的 bigRuleList
             brl.append((freqSet-conseq, conseq, conf))
             #同样需要放入列表到后面检查
